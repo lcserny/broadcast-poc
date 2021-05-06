e73f996f591bbc0494f6debaa6153a7129175909
Leonardo Cserny <leo.cserny@gmail.com>
2021-05-06 18:00:33 +0300

    Initial commit

---
+++ client.js
@@ -0,0 +1,10 @@
+import dgram from "dgram";
+
+let socket = dgram.createSocket("udp4");
+socket.bind(function () {
+    socket.setBroadcast(true);
+});
+let message = Buffer.from("Hi");
+socket.send(message, 0, message.length, 41234, '255.255.255.255', function (err, bytes) {
+    socket.close();
+});
---
+++ server.js
@@ -0,0 +1,15 @@
+import dgram from "dgram";
+
+let server = dgram.createSocket("udp4");
+server.on("error", function (err) {
+    console.log("server error:n" + err.stack);
+    server.close();
+});
+server.on("message", function (msg, rinfo) {
+    console.log("server got: " + msg + " from " + rinfo.address + ":" + rinfo.port);
+});
+server.on("listening", function () {
+    let address = server.address();
+    console.log("server listening " + address.address + ":" + address.port);
+});
+server.bind(41234);
